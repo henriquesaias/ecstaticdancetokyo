788cf5d5f0cfa67e20754913dfc96a903df38cd4
Henrique Saias <henriquesaias@hsaias.com>
2026-01-04 00:40:30 +0000

    cleanup subscribe file

--- src/stripe.js
+++ src/stripe.js
@@ -1,15 +1,11 @@
-import { loadStripe } from "@stripe/stripe-js";
-
-const stripePromise = loadStripe("pk_live_51LidycLkbVO6UUDrArSmM2T5QsKfJz3wv522dxoGBhYy3JqWq2YT5T1sNfxFan8dQjDByt5SdGsOF83s0JQ9xV8W00w4hiad96");
-
-async function subscribe() {
+const subscribe = async () => {
   const res = await fetch(
     "https://emi-stripe-subscriptions.goretuzk.workers.dev/create-checkout-session",
     { method: "POST" }
   );
 
-  const { url } = await res.json();
-  window.location.href = url;
+  const data = await res.json();
+  window.location.href = data;
 }
 
 export { subscribe };
